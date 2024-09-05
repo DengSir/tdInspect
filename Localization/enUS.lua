288620cd2ac4a514a16cb4734f3666f8311a79a6
Dencer <tdaddon@163.com>
2024-09-05 15:24:37 +0800

    feat: 更新Locale

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -4,19 +4,25 @@ if not L then
 end
 -- @locale:language=enUS@
 L = L or {}
-L["Inspect mouseover"] = true
-L["Inspect target"] = true
-L["Last update:"] = true
-L["Show Model"] = true
+L['Inspect mouseover'] = true
+L['Inspect target'] = true
+L['Last update:'] = true
+--[====[ never used ]====]
+--[====[ L['Show Model'] = true ]====]
 -- @end-locale@
 -- @debug@
--- @import@
-L['Show Model'] = true
+-- @import:language=enUS@
+--[====[ never used ]====]
+--[====[ L['Show Model'] = true ]====]
 L['Last update:'] = true
 L['Data source:'] = true
 L['Inspect target'] = true
 L['Inspect mouseover'] = true
 L['iLvl:'] = true
+L['Show character gear list'] = true
+L['Show inspect compare'] = true
+L['Show inspect gear list'] = true
+L['Show talent background'] = true
 -- @locale-fill@
 -- @end-import@
 -- @end-debug@
--- Localization/zhCN.lua
+++ Localization/zhCN.lua
@@ -3,13 +3,19 @@ if not L then
     return
 end
 
--- @import@
-L['Show Model'] = '显示模型'
+-- @import:language=zhCN@
+
+--[====[ never used ]====]
+--[====[ L['Show Model'] = '显示模型' ]====]
 L['Last update:'] = '最后更新：'
 L['Inspect target'] = '观察目标'
 L['Inspect mouseover'] = '观察鼠标悬停目标'
 L['Data source:'] = '数据来源：'
 L['iLvl:'] = true
+L['Show character gear list'] = '显示角色的装备列表'
+L['Show inspect compare'] = '观察是比较装备'
+L['Show inspect gear list'] = '显示被观察者装备列表'
+L['Show talent background'] = '装备列表显示天赋背景'
 -- @locale-fill@
 
 -- @end-import@
