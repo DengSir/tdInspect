ba95f36310e125ae3d360250ae2010d3f2fadc49
Dencer <tdaddon@163.com>
2025-04-10 16:18:03 +0800

    chore: 添加60级附魔数据拉取

--- Scripts/ItemEnchantGen.ts
+++ Scripts/ItemEnchantGen.ts
@@ -5,6 +5,8 @@
  * @Date   : 9/4/2024, 2:23:31 PM
  */
 
+import { ProjectId } from "./util.ts";
+
 interface ItemEnchant {
     isTemporary: boolean;
     enchant: number;
@@ -20,11 +22,23 @@ interface ItemEnchantMap {
 }
 
 class App {
-    async run() {
-        const resp = await fetch('https://nether.wowhead.com/wotlk/data/gear-planner?dv=100');
+    private url: string;
+
+    constructor(projectId: ProjectId) {
+        if (projectId === ProjectId.Wrath) {
+            this.url = 'https://nether.wowhead.com/wotlk/data/gear-planner?dv=100';
+        } else if (projectId === ProjectId.Vanilla) {
+            this.url = 'https://nether.wowhead.com/classic/data/gear-planner?dv=100';
+        } else {
+            throw new Error('not support');
+        }
+    }
+
+    async run(file: string) {
+        const resp = await fetch(this.url);
         const text = await resp.text();
 
-        const m = text.match(/"wow\.gearPlanner\.wrath.enchant",(.+)\)/);
+        const m = text.match(/"wow\.gearPlanner\.\w+.enchant",(.+)\)/);
         if (!m) {
             throw Error('not found');
         }
@@ -35,8 +49,9 @@ class App {
         }
 
         const codes = [];
+        const enchants = Object.entries(d).sort((a, b) => a[1].enchant - b[1].enchant);
 
-        for (const [, v] of Object.entries(d)) {
+        for (const [, v] of enchants) {
             if (v.isTemporary) {
                 continue;
             }
@@ -55,8 +70,9 @@ select(2,...).ItemEnchantMake()
 local n=nil
 ${codes.join('\n')}`;
 
-        Deno.writeTextFileSync('Data/Wrath/ItemEnchant.lua', code);
+        Deno.writeTextFileSync(file, code);
     }
 }
 
-new App().run();
+new App(ProjectId.Wrath).run('Data/Wrath/ItemEnchant.lua');
+new App(ProjectId.Vanilla).run('Data/Vanilla/ItemEnchant.lua');
